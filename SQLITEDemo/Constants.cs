c463a6839ac57f7cf3d1501722255d43e8127c9f
rushi-smb <rushi@smbios.co.in>
2023-03-03 12:49:26 +0530

    Create Database access class to access database - SQLiteConnection

--- SQLITEDemo/Constants.cs
+++ SQLITEDemo/Constants.cs
@@ -12,7 +12,8 @@ namespace SQLITEDemo
         //  The File Name we are going to store in device
         private const string DBFileName = "SQLite.db3"; 
 
-        //  An enumeration type to specify what is going to be the behavior of the file        
+        //  An enumeration type to specify what is going to be the behavior of the file
+        //  How the Connection of file will be open
         public const SQLiteOpenFlags Flags = 
             SQLiteOpenFlags.ReadWrite | 
             SQLiteOpenFlags.Create | 
---
+++ SQLITEDemo/Repositories/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLITEDemo.Repositories
+{
+    public class CustomerRepository
+    {
+        //  Create Connection for local database
+        SQLiteConnection connection;
+
+        public CustomerRepository()
+        {
+            connection =
+                new SQLiteConnection(Constants.DatabasePath,
+                Constants.Flags);
+        }
+    }
+}
