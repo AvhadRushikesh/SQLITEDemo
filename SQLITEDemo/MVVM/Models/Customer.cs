7c4d47ca0679b055bd44cb4ae90e5100d91a0e40
rushi-smb <rushi@smbios.co.in>
2023-03-03 12:28:58 +0530

    Appying SQLite Attribute to Models class

--- SQLITEDemo/MVVM/Models/Customer.cs
+++ SQLITEDemo/MVVM/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,25 @@ using System.Threading.Tasks;
 
 namespace SQLITEDemo.MVVM.Models
 {
+    [Table("Customers")]
     public class Customer
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [Column("name"), Indexed, NotNull]
         public string Name { get; set; }
+
+        [Unique]
         public string Phone { get; set; }
+
         public int Age { get; set; }
+
+        [MaxLength(100)]
         public string Address { get; set; }
+
+        [Ignore]
+        public bool IsYoung =>
+            Age > 50 ? true : false;
     }
 }
